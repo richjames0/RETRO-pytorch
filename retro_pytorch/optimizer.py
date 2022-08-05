be9a9edff086c729ce5ada738929484463f18029
Rich James <hcir@fb.com>
2022-08-05 16:09:47 -0700

    should have thought that last commit through a little more

--- retro_pytorch/optimizer.py
+++ retro_pytorch/optimizer.py
@@ -2,7 +2,7 @@ from torch.optim import AdamW
 
 def separate_weight_decayable_params(params):
     no_wd_params = [param for param in params if param.ndim < 2]
-    wd_params = params - no_wd_params
+    wd_params = [param for param in params if param.ndim >= 2]
     return wd_params, no_wd_params
 
 def get_optimizer(params, lr = 3e-4, wd = 1e-1, filter_by_requires_grad = False):
@@ -12,8 +12,8 @@ def get_optimizer(params, lr = 3e-4, wd = 1e-1, filter_by_requires_grad = False)
     wd_params, no_wd_params = separate_weight_decayable_params(params)
 
     param_groups = [
-        {'params': list(wd_params)},
-        {'params': list(no_wd_params), 'weight_decay': 0},
+        {'params': wd_params},
+        {'params': no_wd_params, 'weight_decay': 0},
     ]
 
     return AdamW(param_groups, lr = lr, weight_decay = wd)
