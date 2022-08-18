b54fc896ce94bbdc91b70752d21e1a96323a9420
Rich James <hcir@fb.com>
2022-08-18 13:42:03 -0700

    commenting out nll etc. for now

--- retro_pytorch/retro_pytorch.py
+++ retro_pytorch/retro_pytorch.py
@@ -661,7 +661,16 @@ class RETRO(nn.Module):
 
         loss = F.cross_entropy(rearrange(logits, 'b n c -> b c n'), labels, ignore_index = self.pad_id)
         # print(loss)
-        input_ = F.log_softmax(logits)
-        nll = F.nll_loss(rearrange(input_, 'b n c -> b c n'), labels, ignore_index = self.pad_id)
+        # input_ = F.log_softmax(logits)
+        # nll = F.nll_loss(rearrange(input_, 'b n c -> b c n'), labels, ignore_index = self.pad_id)
         # print(nll)
-        return loss.unsqueeze(0)
+
+        # model_emissions = rearrange(logits, 'b n c -> b c n')
+        # log_softmax_model_emissions = torch.nn.functional.log_softmax(model_emissions, dim=1)
+        # ground_truth = labels
+        # ce = torch.nn.functional.cross_entropy(model_emissions, ground_truth)
+        # print(ce)
+        # nll = torch.nn.functional.nll_loss(log_softmax_model_emissions, ground_truth)  # .nonzero()[:, 0], reduction='mean')
+        # print(nll)
+
+        return loss.unsqueeze(0)  # TODO: calculate ppl on a per sequence basis
